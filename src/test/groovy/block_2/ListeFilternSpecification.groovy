15368444bbefd4d1ceb2c1c963534fe71bbb70f6
Markus <1921806@stud.hs-mannheim.de>
2026-02-08 20:47:17 +0100

    neue aufgabe zu listen

--- src/test/groovy/block_2/ListeFilternSpecification.groovy
+++ src/test/groovy/block_2/ListeFilternSpecification.groovy
@@ -12,14 +12,6 @@ class ListeFilternSpecification extends Specification {
         ListFilter.oddNumbers(list) == [1, 3, 5]
     }
 
-    def "Aufgabe Alle Zahlen größer als 3 filtern"() {
-        given:
-        def numbers = [1, 2, 3, 4, 5, 6]
-
-        expect:
-        ListFilter.greaterThanThree(numbers) == [4, 5, 6]
-    }
-
     def "Aufgabe Alle Wörter mit mehr als 4 Buchstaben filtern"() {
         given:
         def words = ["Groovy", "Java", "Kotlin", "Go", "Scala"]
@@ -34,5 +26,14 @@ class ListeFilternSpecification extends Specification {
 
         expect:
         ListFilter.processNumbers(numbers) == [9, 25, 49, 81]
+    }
+
+    def "Aufgabe Bestimmte Zahlen auswählen, Duplikate entfernen und sortieren"() {
+        given:
+        def numbers = [6, 2, 4, 2, 6, 1, 5]
+        def allowed = [4, 2, 6]
+
+        expect:
+        ListFilter.selectedUniqueNumbers(numbers, allowed) == [2, 4, 6]
     }
 }
